c6e8b26f59f0532f57296bd29a623554ada0a48a
Sam Larison <samlarison@gmail.com>
2015-09-21 18:28:12 -0500

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Sam Larison on 9/21/15.
+//  Copyright (c) 2015 SamLarison@Vidsbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
